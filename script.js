d898ddb1fcc63b44209aab728f7ee42bbc013785
JulianJulesHernandez <108310215+JulianJulesHernandez@users.noreply.github.com>
2022-07-04 18:20:20 -0700

    changes to a couple files

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+    App Name: First JavaScript Application
+    Author: Julian Hernanadez
+    Date: 07/04/21
+*/
+// Allow the user to enter their guess
+let num = prompt('Guess a number between 1 and 10');
+// Check to see if the number is a winner
+if (num ==5) {
+    //If it is, they got it right
+    alert('You guessed the right number!');
+} else {
+    // if it isn't, they got it wrong!'
+    alert ('You guessed the wrong number!');
+}
